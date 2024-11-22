6af4b877a03e48990da1a26029220bee8a7c4026
Qiming YE <34636290+kimiye1989@users.noreply.github.com>
2024-11-22 14:00:42 +0800

    re-structured
    re-structured

--- initial_mac.sh
+++
@@ -1,9 +0,0 @@
-rm *.xml
-netgenerate --grid --grid.number=5 --grid.length=250 --grid.attach-length=250 -L 2 --output-file=meta.net.xml
-# python C:\SUMO\tools\randomTrips.py -n meta.net.xml -o meta.trips.xml --period=0.20 --binomial=20
-
-# Initialise parking areas & traffic flows/ vehicle_stops
-python3 generate_parking_flows.py
-
-duarouter -n meta.net.xml -r meta.flows.xml -o meta.rou.xml --no-warnings
-python3 vehicle_stops.py
--- initial_win.bat
+++
@@ -1,9 +0,0 @@
-del *.xml
-netgenerate --grid --grid.number=5 --grid.length=250 --grid.attach-length=250 -L 2 --output-file=meta.net.xml
-:: python C:\SUMO\tools\randomTrips.py -n grid.net.xml -o grid.trips.xml --period=0.20 --binomial=20
-python3 generate_parking_flows.py
-
-:: duarouter -n grid.net.xml --additional-files grid.trips.xml -o grid.rou.xml
-duarouter -n meta.net.xml -r meta.flows.xml -o meta.rou.xml --no-warnings
-python3 vehicle_stops.py
-:: sumo -n grid.net.xml -a parking-areas.xml -r grid-stops.rou.xml --tripinfo-output tripinfo.xml
